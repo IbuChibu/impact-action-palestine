e8fa4edce0a8e70d34a1cceb0d21fb454539f792
“Ebrahim <ebrahim@inclusive.energy>
2025-07-20 14:30:17 +0100

    making connections for backend to database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path="backend/.env")
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI!"}
+@app.get("/actions")
+def get_actions():
+    response = supabase.table("activism_actions").select("*").execute()
+    return response.data
